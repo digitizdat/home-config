c41757e976fdbc99005da948a2df31222bca796f
Martin McGreal <mmmcgrea@amazon.com>
2021-06-11 09:28:44 -0500

    Update ipython colors, fix alias typo in bashrc

--- ipython_config.py
+++ ipython_config.py
@@ -1,2 +1,4 @@
 c.TerminalInteractiveShell.editing_mode = 'vi'
-
+c.TerminalInteractiveShell.editor = 'vim'
+c.InteractiveShell.colors = 'linux'
+c.TerminalInteractiveShell.highlighting_style = 'material'
